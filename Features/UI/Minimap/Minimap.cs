1f9ee5cf6623f702fd30b7fd151f0f987adc363d
Zach Alexander <zalexan1996@gmail.com>
2024-06-04 22:46:06 -0500

    Made the minimap toggleable Fixed the ground cannon's pivot

--- Features/UI/Minimap/Minimap.cs
+++ Features/UI/Minimap/Minimap.cs
@@ -9,8 +9,20 @@ public partial class Minimap : Control
 	[Export]
 	public Sprite2D Sprite2D { get; set; }
 
+	[Export]
+	public Panel MinimapPanel { get; set; }
+
+	[Export]
+	public Label ToggleLabel { get; set; }
+
     public override void _Process(double delta)
     {
 		Sprite2D.Position = Player.Position;
+
+		if (Input.IsActionJustReleased("ToggleMinimap"))
+		{
+			ToggleLabel.Visible = !ToggleLabel.Visible;
+			MinimapPanel.Visible = !MinimapPanel.Visible;
+		}
     }
 }
